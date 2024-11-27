063cb4a0a2770c665d80f01633cb1a424006dfcf
flanker2007 <aykmarkosyan@gmail.com>
2024-11-27 17:31:55 +0300

    fast-forward-first-commit

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-x = 10
-y = 15
+st = input()
 
-print(x * y)
-print(x + y)
+x = tuple(set(st.split()))
+y = tuple(st.split())
+
+lst = []
+
+for i in x:
+    count = 0
+    for j in y:
+        if(i == j):
+            count += 1
+    sent = f"Слово {i} встретилось в вашем тексте {str(count)} раз"
+    lst.append(sent)
+
+print(f"В вашем тексте {len(x)} различных слов")
+
+for i in lst:
+    print(i)
