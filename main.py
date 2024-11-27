c3936f370f716145ecb53f02c9cdb2d690b24d5a
flanker2007 <aykmarkosyan@gmail.com>
2024-11-27 17:33:47 +0300

    fast-forward-third-commit

--- main.py
+++ main.py
@@ -26,3 +26,24 @@ example_set = set(example.split())
 
 print(len(user_set & example_set))
 print(round((len(user_set - example_set) / len(user_set)) * 100), "%", sep = "")
+
+st = input()
+
+st_split = st.split()
+
+lst_numbers = ["ноль","один","два","три","четыре","пять","шесть","семь",
+               "восемь","девять","десять","одиннадцать","двенадцать",
+               "тринадцать","четырнадцать","пятнадцать","шестнадцать",
+               "семнадцать","восемнадцать","девятнадцать","двадцать"]
+lst_sings = ["минус", "плюс"]
+
+if(st_split[1] == lst_sings[0]):
+    if(lst_numbers.index(st_split[0]) >= lst_numbers.index(st_split[2])):
+        result = lst_numbers.index(st_split[0]) - lst_numbers.index(st_split[2])
+        print(lst_numbers[result])
+    else:
+        result = lst_numbers.index(st_split[2]) - lst_numbers.index(st_split[0])
+        print(st_split[1], lst_numbers[result])
+else:
+    result = lst_numbers.index(st_split[0]) + lst_numbers.index(st_split[2])
+    print(lst_numbers[result])
